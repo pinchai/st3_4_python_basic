dfb815b7657677248804d7301d5074e143247781
Pinchai <>
2024-04-23 13:55:33 +0700

    add pip

--- user_input_and_variable.py
+++ user_input_and_variable.py
@@ -1,11 +1,7 @@
-name = input("Enter Your Name(Max 12 character): ") #max: 12
-gender = input("Enter Your Gender: ") #male and female
-age = input("Enter Your Age: ") #only number, 15-35
-address = input("Enter Your Address: ")
-print('==============================')
-print("your name is: "+name)
-print("your gender is: "+gender)
-print("your gender is: "+age)
-print("your address is: "+address)
+from prettytable import PrettyTable
+table = PrettyTable()
 
+table.field_names = ["ID", "Name", "Gender", "Address"]
+table.add_row(["STD0001", "srey neath", 'female', "Takeo"])
 
+print(table)
